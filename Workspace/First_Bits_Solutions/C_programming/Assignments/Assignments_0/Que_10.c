f9cc1b3968ac456846c4d9dad918e1c9128d2c05
kiranzagade <kiranzagade3181@gmail.com>
2026-02-04 21:05:25 +0530

    Add files via upload

---
+++ Workspace/First_Bits_Solutions/C_programming/Assignments/Assignments_0/Que_10.c
@@ -0,0 +1,16 @@
+void main (){
+	int java,c,cpp,sql,python,avg,marks_obt;
+	float percentage,total_marks = 500;
+	java = 90;
+	c = 85;
+	cpp = 95;
+	sql = 80;
+	python = 99;
+	marks_obt = java+c+cpp+sql+python;
+	avg = marks_obt/5;
+	percentage = (marks_obt/total_marks)*100.00;
+	printf("The average of marks and the percentage is %d %f
+	",marks_obt,percentage);
+	
+	
+}
---
+++ Workspace/First_Bits_Solutions/C_programming/Assignments/Assignments_0/Que_5.c
@@ -0,0 +1,12 @@
+void main(){
+	int num1,num2,num3,num4,num5;
+	float avg;
+	num1 = 15;
+	num2 = 54;
+	num3 = 46;
+	num4 = 19;
+	num5 = 37;
+	avg = (num1+num2+num3+num4+num5)/5;
+	printf("The average of 5 numbers is = %f",avg);
+	
+}
