7b0966ab9da1e4d5893a7db2db6c70136360cf9a
kiranzagade <kiranzagade3181@gmail.com>
2026-03-05 12:57:45 +0530

    Que-_10.C

--- Workspace/First_Bits_Solutions/C_programming/Assignments/Assignment_8/Que-_10.C
+++ Workspace/First_Bits_Solutions/C_programming/Assignments/Assignment_8/Que-_10.C
@@ -4,15 +4,18 @@
 int main(){
 	int arr[10] = {12,32,45,65,78,98,41,36,85,20};
 	int size = sizeof(arr)/sizeof(int);
-	for(int i = 0;i<size;i++){
-		for(int j = 0;j<size;j++){
-			if(arr[i]>arr[j+1]){
-				arr[i] = arr[j+1];
-			}
+	for(int i=0;i<size;i++){
+		for(int j=i+1;j<size;j++){
+			if(arr[i]>arr[j]){
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
 		}
-	}
-	for(int i = 0;i<size;i++){
-		printf("%d ",arr[i]);
+		}
+		
 	}
 	
+	for(int i=0;i<size;i++){
+		printf("%d ",arr[i]);
+	}
 }
